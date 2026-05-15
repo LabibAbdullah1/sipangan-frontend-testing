2c6e2bf709d5515c691270dcc4c49a3aac05a56e
Labib Abdullah <labibabdullahhasan@gmail.com>
2026-05-15 07:02:15 +0700

    feat: configure .htaccess for SPA routing and update Vite base path to root

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Menggunakan path relatif agar aman di berbagai environment hosting (CPanel/Vercel)
+  base: '/', // Menggunakan path relatif agar aman di berbagai environment hosting (CPanel/Vercel)
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
